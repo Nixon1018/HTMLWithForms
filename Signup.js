30b246f71fbc57131f4e1e3cadb1f0420f3498b0
Nixon1018 <123439949+Nixon1018@users.noreply.github.com>
2023-03-10 17:33:11 +0530

    Update Signup.js

--- Signup.js
+++ Signup.js
@@ -23,7 +23,7 @@ const setSuccess = element => {
 };
 
 const isValidEmail = (email) => {
-    const re =/\S+@\S+\.\S+/;
+    const re =/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm;
     return re.test(String(email).toLowerCase())
 }
 const isValidUsername = (username) => {
